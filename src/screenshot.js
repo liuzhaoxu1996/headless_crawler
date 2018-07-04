3865a985385476741960dc68766bf40385d7b29e
liuzhaoxu1996 <1097959570@qq.com>
2018-07-04 23:05:30 +0800

    :apple: fist commit
    first commit

---
+++ src/screenshot.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+const {
+  screenshot
+} = require('./config/default.js');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.taobao.com');
+  await page.screenshot({
+    path: `${screenshot}/${Date.now()}.png`
+  });
+  await browser.close();
+})();
